a71d06f9cf8d0acd99b0273516b0357a689b1d66
andreyukD <wushu_dima@mail.ru>
2019-01-13 16:30:42 +0100

    installed babel, created empty js search files, tested import/export modules, tested api call food2fork

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,27 @@
-// Global app controller
-import x from './test';
-console.log(`Test webpack ${x}`);
+/*
+import mystring from './models/Search';
+import { add as a, mult as m, ID } from './views/searchView';
+import * as allSearchView from './views/searchView';
+
+console.log(mystring);
+console.log(`${a(ID, ID)} and ${allSearchView.mult(ID, ID)}`);
+console.log(allSearchView);
+*/
+
+//869c52dcbd9527613402db6b527a7c66 https://www.food2fork.com/user/api
+//https://www.food2fork.com/api/search
+
+import axios from 'axios';
+async function getResuls(query) {
+    const key = `869c52dcbd9527613402db6b527a7c66`;
+    try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    }
+    catch(error) {
+        alert(error);
+    }
+}
+getResuls('ice cream');
+
